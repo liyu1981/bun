efad63f9640f4db900c94c2b81f3fbcb9a5515dd
Yu Li <liyu1981@gmail.com>
2023-12-14 14:33:20 +1100

    do not use the global dir_cache in bundler thread, as when auto_install it will cause problem

--- src/bundler/bundle_v2.zig
+++ src/bundler/bundle_v2.zig
@@ -127,6 +127,7 @@ const JSC = bun.JSC;
 const debugTreeShake = Output.scoped(.TreeShake, true);
 const BitSet = bun.bit_set.DynamicBitSetUnmanaged;
 const Async = bun.Async;
+const DirInfo = @import("../resolver/dir_info.zig");
 
 fn tracer(comptime src: std.builtin.SourceLocation, comptime name: [*:0]const u8) bun.tracy.Ctx {
     return bun.tracy.traceNamed(src, "Bundler." ++ name);
@@ -1618,6 +1619,10 @@ pub const BundleV2 = struct {
         bundler.options.global_cache = config.install;
         bundler.resolver.opts.global_cache = config.install;
         bundler.resolver.env_loader = bundler.env;
+        // in thread we use a seperate dirinfo cache inited on thread arena
+        // allocator as resolver.zig:597 always use bun.default_allocator,
+        // which will cause when bundle in thread & auto_install crash
+        bundler.resolver.dir_cache = DirInfo.HashMap.init_force(allocator);
 
         bundler.options.jsx = config.jsx;
         bundler.options.no_macros = config.no_macros;
